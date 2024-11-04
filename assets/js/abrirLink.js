01dcd10747c7a46ed962c6460c3997418bfd200d
Caio Rodrigo <caiorodrigocev@gmail.com>
2024-11-03 22:39:30 -0300

    Comentario no AbrirLink.js pronto para descomentar assim que o servidor tiver com https

--- assets/js/abrirLink.js
+++ assets/js/abrirLink.js
@@ -3,12 +3,12 @@ function abrirLink() {
         if (link) {
           if (link.includes("youtube.com")||link.includes(("youtu.be"))) {
               var encodedLink = encodeURIComponent(link);
-               window.open('http://89.117.33.245:8000/download/'+ encodedLink, '_blank');
+               window.open('http://192.168.2.110:8000/download/'+ encodedLink, '_blank');
 
                // Descomentar os dois abaixo para abrir o video na mesma tela abaixo do botao baixar
                
                //document.getElementById('downloadFrame').style.display = 'block';
-               //document.getElementById('downloadFrame').src = 'http://89.117.33.245:8000/download/' + encodedLink;
+               //document.getElementById('downloadFrame').src = 'http://192.168.2.110:8000/download/' + encodedLink;
           } else {
               alert("Por favor, insira um link válido do YouTube.");
           }             
