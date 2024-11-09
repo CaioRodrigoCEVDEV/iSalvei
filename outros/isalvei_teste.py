2399fad028492554f0de61ea7466cbb91ff73fda
Caio Rodrigo <caiorodrigocev@gmail.com>
2024-11-09 14:48:18 -0300

    Implementado API X

--- outros/isalvei_teste.py
+++ outros/isalvei_teste.py
@@ -28,24 +28,33 @@ with open('/etc/api_key.txt', 'r') as y:
     API_KEY = y.read().strip()
 with open('/etc/api_key_i.txt', 'r') as i:
     API_KEY_I = i.read().strip()
+with open('/etc/api_key_x.txt','r') as x:
+    API_KEY_X = x.read().strip()
+with open('/etc/api_key_x_secret.txt','r') as x_secret:
+    API_KEY_X_SECRET = x_secret.read().strip()
+with open('/etc/api_key_x_token.txt','r') as x_token:
+    API_KEY_X_TOKEN = x_token.read().strip()
+with open('/etc/api_key_x_token_secret.txt','r') as x_token_secret:
+    API_KEY_X_TOKEN_SECRET = x_token_secret.read().strip()
+
 class RequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path.startswith('/download/'):
             # Extrair a URL do vídeo diretamente da rota
             video_url = urllib.parse.unquote(self.path[len('/download/'):])
             ydl_opts = {}
-            if 'instagram.com' in video_url:
+            if 'instagram.com' in video_url or 'facebook.com' in video_url:
                 ydl_opts = {
-                'format': 'bestvideo[ext=mp4]',
+                'format':  'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]',
                 'outtmpl': '/var/www/html/download/%(id)s.%(ext)s',
                 'format': 'best',                    
                 'api_key': API_KEY_I,
                 'sleep_interval': 5,
-                'max_sleep_interval': 15,
+                'max_sleep_interval': 15
                 }
             elif 'youtube.com' in video_url or 'youtu.be' in video_url:
                     ydl_opts = {
-                        'format': 'bestvideo[ext=mp4]',
+                        'format': 'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]',
                         'outtmpl': '/var/www/html/download/%(id)s.%(ext)s',
                         'format': 'best',
                         'quiet': True,
@@ -55,8 +64,24 @@ class RequestHandler(SimpleHTTPRequestHandler):
                         'cookiefile': '/var/www/isalvei/ytt_cookies.txt',
                         #'max_duration': '60m',
                         #'cookiesfrombrowser': ('chrome',),
+                        'sleep_interval': 5,
+                        'max_sleep_interval': 15,
                         'api_key': API_KEY
                     }
+                 
+            elif 'x.com' in video_url or 'twitter.com' in video_url:
+                  ydl_opts = {
+                    'format': 'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]',
+                    'outtmpl': '/var/www/html/download/%(id)s.%(ext)s',
+                    'sleep_interval': 5,
+                    'max_sleep_interval': 15,
+                    'twitter_api': {
+                        'app_key': API_KEY_X,
+                        'app_secret':API_KEY_X_SECRET,
+                        'oauth_token': API_KEY_X_TOKEN,
+                        'oauth_token_secret': API_KEY_X_TOKEN_SECRET
+                     }
+                   }
 
             try:
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
