f69ef378f7702a27e5f1bb986fba48613b14c6e7
Caio Rodrigo <caiorodrigocev@gmail.com>
2024-11-18 21:45:20 -0300

    valida link

--- assets/js/abrirLink.js
+++ assets/js/abrirLink.js
@@ -3,18 +3,9 @@ function abrirLink() {
         if (link) {
           if (link.includes("youtube.com")||link.includes(("youtu.be"))) {
               var encodedLink = encodeURIComponent(link);
-              
-              //window.open('http://127.0.0.1:8000/download/'+ encodedLink, '_blank');
-
-               // Descomentar os dois abaixo para abrir o video na mesma tela abaixo do botao baixar
                
-<<<<<<< HEAD
-               document.getElementById('downloadFrame').style.display = 'block';
-               document.getElementById('downloadFrame').src = 'http://127.0.0.1:8000/download/' + encodedLink;
-=======
-               //document.getElementById('downloadFrame').style.display = 'block';
-               //document.getElementById('downloadFrame').src = 'https://89.117.33.245:8000/download/' + encodedLink;
->>>>>>> self_page
+             // window.open('http://127.0.0.1:8000/download/'+ encodedLink, '_blank');
+
           } else {
               alert("Por favor, insira um link válido do YouTube.");
           }             
